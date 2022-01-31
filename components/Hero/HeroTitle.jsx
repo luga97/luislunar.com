6cd6b171ac05c438364904e2ed1caf7b986ff7df
Luis David Garcia Lunar <ldgl1215@gmail.com>
2022-01-30 21:37:45 -0300

    fixing hero text

--- components/Hero/HeroTitle.jsx
+++ components/Hero/HeroTitle.jsx
@@ -1,6 +1,6 @@
 const HeroTitle = () => (
   <div>
-    <h1 className='font-blod text-5xl lg:textl-7xl text-center lg:text-left'>Building Amazing and Functionals Softwares </h1>
+    <h1 className='font-blod text-4xl md:text-5xl lg:textl-7xl text-center lg:text-left'>Building Amazing and Functionals Softwares </h1>
   </div>
 );
 
