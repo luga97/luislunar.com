751fe1a833cc68b21bfb8c8a75bf277c26a48672
Luis David Garcia Lunar <ldgl1215@gmail.com>
2022-01-30 21:44:49 -0300

    fixing hero text size

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -17,7 +17,7 @@ const Hero = () => (
   <div className="container mt-16 flex justify-between items-center mx-auto px-8 md:px-14">
     <div className="flex flex-wrap lg:flex-nowrap justify-between w-full">
         <HeroNav/>
-        <div className="flex flex-wrap grow lg:grow-0 lg:flex-shrink xl:ml-20 justify-center md:justify-center lg:justify-start max-w-xl mt-0 md:my-36">
+        <div className="flex flex-wrap grow justify-center mt-0 md:justify-center  md:my-36 lg:grow-0 lg:flex-shrink lg:justify-start xl:ml-20 ">
           <HeroTitle/>
           <HeroButton/>          
         </div>
